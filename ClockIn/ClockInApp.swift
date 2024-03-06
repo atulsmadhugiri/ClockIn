5593b58bfc530e0d53085381dd5e50ebce2a5991
Atul Madhugiri <atulm@berkeley.edu>
2024-03-06 03:11:43 -0500

    [macos] Remove SwiftData-related code
    We can skip persistence and instead avoid duplicate messages by
    - maintaining `lastSent` state in `ContentView` while app is live
    - setting `localID` (effectively nonce) to stringified `Date()` to avoid resending if we close/open app and `lastSent` state gets reset

--- ClockIn/ClockInApp.swift
+++ ClockIn/ClockInApp.swift
@@ -1,23 +1,10 @@
-import SwiftData
 import SwiftUI
 
 @main
 struct ClockInApp: App {
-  var sharedModelContainer: ModelContainer = {
-    let schema = Schema([])
-    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-    do {
-      return try ModelContainer(for: schema, configurations: [modelConfiguration])
-    } catch {
-      fatalError("Could not create ModelContainer: \(error)")
-    }
-  }()
-
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
-    .modelContainer(sharedModelContainer)
   }
 }
