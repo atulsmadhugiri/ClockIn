34b85d4e99f3f1781beda5a758f29621b3591c21
Atul Madhugiri <atulm@berkeley.edu>
2024-03-08 18:21:56 -0500

    [cli] Naive implementation of `NetworkManager` in `ClockInCLI`
    Need to handle weekends, redundant requests, etc. But we have the basics working.

--- ClockInCLI/main.swift
+++ ClockInCLI/main.swift
@@ -1,3 +1,31 @@
 import Foundation
+import Network
 
-print("Hello, World!")
+class NetworkMonitor {
+  private let monitor: NWPathMonitor = NWPathMonitor(requiredInterfaceType: .wifi)
+  private let queue = DispatchQueue(label: "ClockInCLIQueue")
+  private var updateLastSentAt = Date(timeIntervalSince1970: 0)
+  private var isConnected: Bool = false
+
+  init() {
+    monitor.pathUpdateHandler = { path in
+      if path.status == .satisfied {
+        guard !self.isConnected else { return }
+        self.isConnected = true
+        if getCurrentSSID() == Constants.OFFICE_SSID {
+          Task {
+            try await Task.sleep(nanoseconds: 3 * 1_000_000_000)
+            try await sendMessage(text: "Atul is here.")
+          }
+        }
+      } else {
+        self.isConnected = false
+      }
+    }
+    monitor.start(queue: queue)
+  }
+
+}
+
+let networkMonitor = NetworkMonitor()
+RunLoop.main.run()
